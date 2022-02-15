766d66831b7edc3c8ce498132089e41464df3bed
gauravsarwal <gauravsarwal1487@gmail.com>
2022-02-15 12:27:12 +0530

    basket microservices developed

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,18 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString").ToString());
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName").ToString());
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName").ToString());
+            CatalogContextSeed.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
